f4e17da8d0eb9585eea182573102b0bd0eaee539
Harold C <hcabalic@gmail.com>
2023-03-26 00:40:24 -0700

    Add tailwind and react-hook-form

--- src/components/request-form/RequestForm.tsx
+++ src/components/request-form/RequestForm.tsx
@@ -1,8 +1,29 @@
+import { useForm } from "react-hook-form";
+
+type FormData = {
+  firstName: string;
+  lastName: string;
+}
+
 export default function RequestForm() {
+  const { register, setValue, handleSubmit, formState: { errors } } = useForm<FormData>();
+  const onSubmit = handleSubmit(data => console.log(data));
   return (
     <>
       <h2>Request Form</h2>
-      <p>This is the Request Form Page</p>
+      <form onSubmit={onSubmit}>
+        <label>First Name</label>
+        <input {...register("firstName")} />
+        <label>Last Name</label>
+        <input {...register("lastName")} />
+        <button
+          type="button"
+          onClick={() => {
+            setValue("lastName", "Cabalic");
+            setValue("firstName", "Harold");
+          }}  
+        >Submit</button>
+      </form>
     </>
   )
 }
