7f258b940cc5965841cd5df3baf986a03239080a
Harold C <hcabalic@gmail.com>
2023-03-26 23:36:48 -0700

    Add tailwind and some test styles

--- src/components/request-form/RequestForm.tsx
+++ src/components/request-form/RequestForm.tsx
@@ -1,8 +1,21 @@
 import { useForm } from "react-hook-form";
+import './request-form.css';
 
 type FormData = {
-  firstName: string;
-  lastName: string;
+  ldap: string;
+  team: string;
+  dataset_name: string;
+  description: string;
+  service: string[];
+  storage_type: string[];
+  qps_get: number;
+  qps_set: number;
+  qps_delete: number;
+  is_pii: boolean;
+  criticalness: number[];
+  cluster: string[];
+  num_partitions: number;
+  review_status: string[];
 }
 
 export default function RequestForm() {
@@ -11,16 +24,17 @@ export default function RequestForm() {
   return (
     <>
       <h2>Request Form</h2>
-      <form onSubmit={onSubmit}>
-        <label>First Name</label>
-        <input {...register("firstName")} />
-        <label>Last Name</label>
-        <input {...register("lastName")} />
+      <form className="dataset__request-form" onSubmit={onSubmit}>
+        <label>User LDAP</label>
+        <input {...register("ldap")} />
+        <label>Team Name</label>
+        <input {...register("team")} />
         <button
+          className="btn"
           type="button"
           onClick={() => {
-            setValue("lastName", "Cabalic");
-            setValue("firstName", "Harold");
+            setValue("ldap", "mnguyen");
+            setValue("team", "kv-systems");
           }}  
         >Submit</button>
       </form>
